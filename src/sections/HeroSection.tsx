8467d6e7b645d5768de208c421cd91fbcd53a259
orlaa24ad@pm.me <orlaa24ad@pm.me>
2026-07-05 18:46:42 +0200

    fixed: fonts size HeroSection

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -6,13 +6,15 @@ function HeroSection() {
   return (
     <section
       className="min-h-svh bg-negro flex items-start justify-left px-70 pt-50 bg-cover border-b-1 border-dorado relative"
-      style={{ backgroundImage: `url(${Pose})`, backgroundPosition: 'left 280px center' }}
+      style={{ backgroundImage: `url(${Pose})`, backgroundPosition: 'left 200px center' }}
     >
       <div className="absolute inset-0 bg-gradient-to-r from-negro/80 to-transparent" />
       <div className="relative z-10 max-w-2xl space-y-6 text-center">
         <div className="relative">
           <p className="font-serif text-piedra text-base tracking-widest uppercase border-b-1 border-dorado pb-10">
-            BIENVENIDO AL ARCHIVO OFICIAL DE
+            <h1 className="text-3xl">
+              FRANCO REINALDO POU
+            </h1>
           </p>
           <Astroid className="absolute left-1/2 -translate-x-1/2 -bottom-3 text-dorado h-6 w-6" />
         </div>
@@ -22,7 +24,7 @@ function HeroSection() {
           </h1>
           <Astroid className="absolute left-1/2 -translate-x-1/2 -bottom-3 text-dorado h-6 w-6" />
         </div>
-        <p className="font-serif text-piedra text-xl leading-relaxed">
+        <p className="font-serif text-piedra text-3xl leading-relaxed">
           Toda epoca tiene un autor que la define. <br /> <span className="text-dorado italic">Esta decidio exagerar.</span>
         </p>
         <Button to="/obra-maestra">Entrar al archivo</Button>
